8fcbf70a7b06b0dcc32fae274ca003bd414aa2c3
David Speck <david.speck@wooga.net>
2018-06-26 21:58:01 +0200

    Data: Vectorize ingredients and cusines

--- cooking_script.py
+++ cooking_script.py
@@ -1,6 +1,34 @@
 import pandas as pd
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 print("Reading data...")
 train = json.load(open("./input/train.json"))
 test = json.load(open("./input/test.json"))
+
+# prepare data
+labels = []
+text_data = []
+for recipe in train:
+    text_data.append(" ".join(recipe["ingredients"]))
+    if recipe["cuisine"] not in labels:
+        labels.append(recipe["cuisine"])
+
+for recipe in test:
+    text_data.append(" ".join(recipe["ingredients"]))
+
+join = " ".join(test[0]['ingredients'])
+
+# create vectorizer
+vectorizer = TfidfVectorizer()
+vectorizer.fit( text_data )
+# create map for labels
+label_to_int = dict(((c, i) for i, c in enumerate(labels)))
+int_to_label = dict(((i, c) for i, c in enumerate(labels)))
+
+Y_train = []
+X_train = []
+
+for recipe in train:
+    Y_train.append( label_to_int[recipe["cuisine"]] )
+    X_train.append( vectorizer.transform( " ".join(recipe["ingredients"]) ))
